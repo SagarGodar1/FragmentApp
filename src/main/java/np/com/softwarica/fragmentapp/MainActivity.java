8449f96d23cc41786f3c25ca7d187ee0b26c54a7
Softwarica College Lab <softwarica@softwarica.com>
2019-04-21 11:28:55 +0545

    Final Product

--- src/main/java/np/com/softwarica/fragmentapp/MainActivity.java
+++ src/main/java/np/com/softwarica/fragmentapp/MainActivity.java
@@ -1,13 +1,55 @@
 package np.com.softwarica.fragmentapp;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import Fragment.FirstFragment;
+import Fragment.SecondFragment;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private Button btnFragment;
+
+    private Boolean status = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnFragment = findViewById(R.id.btnFragment);
+        btnFragment.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if (status){
+
+            FirstFragment firstFragment = new FirstFragment();
+            fragmentTransaction.replace(R.id.fragmentContainer,firstFragment);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+            btnFragment.setText("load Second Fragment");
+            status = false;
+        }
+        else{
+
+            SecondFragment secondFragment = new SecondFragment();
+            fragmentTransaction.replace(R.id.fragmentContainer,secondFragment);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+            btnFragment.setText("load Second Fragment");
+            status = true;
+        }
     }
 }
